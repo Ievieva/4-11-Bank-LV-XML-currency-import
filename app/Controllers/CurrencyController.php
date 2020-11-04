ceaa3828437f98725fe65c69850f1f6f04ce09ba
Ievieva <ieakalnina@gmail.com>
2020-11-04 21:58:10 +0200

    Initial commit

---
+++ app/Controllers/CurrencyController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Currency;
+
+class CurrencyController
+{
+    public static function add(string $id, string $rate)
+    {
+        $query = query();
+        if (self::getById($id) == null) {
+            $query->insert('currency')
+                ->values([
+                    'id' => ':id',
+                    'rate' => ':rate'
+                ])
+                ->setParameters([
+                    'id' => $id,
+                    'rate' => $rate
+                ])
+                ->execute();
+        }
+    }
+
+    public static function index(): array
+    {
+        $query = query()
+            ->select('*')
+            ->from('currency')
+            ->execute()
+            ->fetchAllAssociative();
+
+        $currencies = [];
+
+        foreach ($query as $currency) {
+            $currencies[] = new Currency(
+                $currency['id'],
+                $currency['rate']
+            );
+        }
+        return $currencies;
+    }
+
+    public static function getById(string $id): Currency
+    {
+        $query = query()
+            ->select('*')
+            ->from('currency')
+            ->where('id = :id')
+            ->setParameter('id', $id)
+            ->execute()
+            ->fetchAssociative();
+
+        return new Currency(
+            $query['id'],
+            $query['rate']
+        );
+    }
+
+    public static function update(string $id, string $rate)
+    {
+        if ((self::getById($id))['rate'] !== $rate) {
+            query()->update('currency')
+                ->set('rate', ':rate')
+                ->setParameter('rate', $rate)
+                ->where('id = :id')
+                ->setParameter('id', $id)
+                ->execute();
+        }
+    }
+}
+
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Controllers\CurrencyController;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Query\QueryBuilder;
+
+session_start();
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+function database(): Connection
+{
+    $connectionParams = [
+        'dbname' => $_ENV['DB_DATABASE'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'host' => $_ENV['DB_HOST'],
+        'driver' => 'pdo_mysql',
+    ];
+
+    $connection = DriverManager::getConnection($connectionParams);
+    $connection->connect();
+
+    return $connection;
+}
+
+function query(): QueryBuilder
+{
+    return database()->createQueryBuilder();
+}
+
+$xml = file_get_contents('https://www.bank.lv/vk/ecb.xml');
+$service = new Sabre\Xml\Service();
+$service->elementMap = [
+    '{http://www.bank.lv/vk/LBCurrencyRates.xsd}Currency' => function (Sabre\Xml\Reader $reader) {
+        return Sabre\Xml\Deserializer\keyValue($reader, 'http://www.bank.lv/vk/LBCurrencyRates.xsd');
+    },
+    '{http://www.bank.lv/vk/LBCurrencyRates.xsd}Currencies' => function (Sabre\Xml\Reader $reader) {
+        return Sabre\Xml\Deserializer\repeatingElements($reader, '{http://www.bank.lv/vk/LBCurrencyRates.xsd}Currency');
+    },
+];
+
+$result = ($service->parse($xml))[1]['value'];
+
+//var_dump($result['0']['ID']);die();
+
+foreach ($result as $currency) {
+    CurrencyController::add($currency['ID'], $currency['Rate']);
+}
+
+$currencyRates = CurrencyController::index();
+
+foreach ($currencyRates as $currency) : ?>
+
+    <p>
+        <strong><?php echo $currency->id(); ?></strong>
+        <?php echo $currency->rate(); ?>
+    </p>
+
+<?php endforeach; ?>
+
